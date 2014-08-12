a672ae54d35e3f2e3cee0293223730706f8591c8
Enrico Bahls <enrico@debiandev>
2014-08-12 14:46:49 +0200

    fixed untracked files

--- Release/arduino/WMath.cpp.d
+++
@@ -1,2 +0,0 @@
-arduino/WMath.cpp.d arduino/WMath.cpp.o:  \
- /home/enrico/Entwicklung/C/tools/arduino-1.5.5/hardware/arduino/avr/cores/arduino/WMath.cpp
--- Release/arduino/hooks.c.d
+++
@@ -1,2 +0,0 @@
-arduino/hooks.c.d arduino/hooks.c.o:  \
- /home/enrico/Entwicklung/C/tools/arduino-1.5.5/hardware/arduino/avr/cores/arduino/hooks.c
